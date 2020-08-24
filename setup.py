1db40e4ccd0443d2a2d8618fc9e61bc821da7b02
Bart Thate <bthate@dds.nl>
2020-08-24 13:33:58 +0200

    up version

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ from setuptools import setup
 
 setup(
     name='oklib',
-    version='4',
+    version='5',
     url='https://github.com/bthate/oklib',
     author='Bart Thate',
     author_email='bthate@dds.nl',
     description="OKLIB is a library with basic console/clock/parse/handle functionality. no copyright or LICENSE. placed in the Public Domain.",
     long_description="OKLIB has all you need to write a standard application.",
     license='Public Domain',
-    install_requires=["olib>=2"],
+    install_requires=["olib>=4"],
     zip_safe=False,
     packages=["ok"],
     namespace_packages=["ok"],
